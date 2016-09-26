3de3bdc9540ccdf2f0865740b976263e0a07bb56
Bhawani Parajuli and Shradha Pulla <bhawani+shradha@>
2016-09-26 11:57:00 -0700

    Working on creating a google map using lat & lng input

--- src/GoogleMaps-Basic/Controllers/HomeController.cs
+++ src/GoogleMaps-Basic/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GoogleMaps_Basic.Models;
+using Google;
 
 namespace GoogleMaps_Basic.Controllers
 {
@@ -12,6 +13,20 @@ namespace GoogleMaps_Basic.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMap(int latitude, int longitude)
+        {
+            var mapCanvas = document.getElementById("map");
+            var mapOptions =
+            {
+                center: new google.maps.LatLng(latitude, longitude),
+                zoom: 16
+            }
+
+            var newMap = new google.maps.Map(mapCanvas, mapOptions);
+            return View(newMap);
         }
     }
 }
--- src/GoogleMaps-Basic/Models/Map.cs
+++ src/GoogleMaps-Basic/Models/Map.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GoogleMaps_Basic.Models
 {
+    [Table("Maps")]
     public class Map
     {
+        [Key]
+        public int Id { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
 
+        public Map (int latitude, int longitude, int id = 0)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Id = id;
+        }
+
+        //public int zoom { get; set; }
     }
+    //public static Map GetMap ()
 }
